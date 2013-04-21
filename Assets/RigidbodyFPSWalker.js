32adf27598de10ffd8c05e3d0d65d3fa8599b41c
Joel Thornton <github@joelpt.net>
2013-04-21 02:37:23 -0700

    Get a reasonable facsimile of skiing, jetpacking, and walking functional.

--- Assets/RigidbodyFPSWalker.js
+++ Assets/RigidbodyFPSWalker.js
@@ -2,29 +2,90 @@ var speed = 10.0;
 var gravity = 10.0;
 var maxVelocityChange = 10.0;
 var canJump = true;
-var jumpHeight = 2.0;
+var jumpHeight = 1.5;
 private var grounded = false;
- 
+var walkingMaterial : UnityEngine.PhysicMaterial;
+var skiingMaterial : UnityEngine.PhysicMaterial;
+var skiingBoostPercent = 0.05;
+var skiingBoostBelowVelocity = 1.0;
+var skiingDampeningFactor = 0.02;
+var fuelAirControlFactor = 0.5;
+var fuelAirControlPenaltyFactor = 0.5;
+var freeAirControlFactor = 0.2;
+
 @script RequireComponent(Rigidbody, CapsuleCollider)
  
 function Awake ()
 {
 	rigidbody.freezeRotation = true;
-	rigidbody.useGravity = false;
+	//rigidbody.useGravity = false;
+	
 }
+
+function GetGroundNormal() {
+	var dist = 1;
+    var dir : Vector3 = Vector3(0,-1,0);
+ 	var hit : UnityEngine.RaycastHit;
+ 	
+    //edit: to draw ray also//
+    
+    //end edit//
  
+ 	if (Physics.Raycast(transform.position, dir, hit, dist)) {
+ 		//Debug.DrawRay(hit.point, hit.normal, Color.red);
+ 		return hit.normal;
+ 	}
+ 	else {
+ 		return null;
+ 	}
+//    if(Physics.Raycast(transform.position,dir,hit,dist)) {
+//         //the ray collided with something, you can interact
+//         // with the hit object now by using hit.collider.gameObject
+//         return hit.collider.gameObject;
+//    }
+//    else {
+//         //nothing was below your gameObject within 10m.
+//         return null;
+//    }
+	return null;
+}
+     	
+	  
 function FixedUpdate ()
 {
+	if (Input.GetMouseButton(0)) {
+		Screen.lockCursor = true;
+//		collider.material = walkingMaterial;
+	}
+	
 	//if (grounded)
 	//{
 
+	var groundNormal = GetGroundNormal();
+	//grounded = (groundNormal != null);
+	
 	// Calculate how fast we should be moving
 	var targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 	targetVelocity = transform.TransformDirection(targetVelocity);
 	targetVelocity *= speed;
 
 	var skiing = Input.GetButton("Jump");
+	var jetpacking = Input.GetMouseButton(1);
 	
+	if (skiing || !grounded || jetpacking) {
+		if (collider.material != skiingMaterial) {
+			collider.material = skiingMaterial;
+		}
+	}
+	else {
+		if (collider.material != walkingMaterial) {
+			collider.material = walkingMaterial;
+		}
+	}
+	
+	
+	//grounded = true;
+	//Debug.Log(grounded);
 	if (grounded && !skiing) {
 		// Apply a force that attempts to reach our target velocity
 		var velocity = rigidbody.velocity;
@@ -32,20 +93,44 @@ function FixedUpdate ()
 		velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
 		velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
 		velocityChange.y = 0;
+		
+		if (groundNormal != null) {
+			velocityChange = AdjustGroundVelocityToNormal(velocityChange, groundNormal);
+		}
+		
 		rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 	}
-	else {
-		// Apply a force that attempts to accelerate in the desired direction
-		rigidbody.AddForce(targetVelocity, ForceMode.Acceleration);
+//	else if (!grounded) {
+//		// Apply a force that attempts to accelerate in the desired direction
+//		rigidbody.AddForce(targetVelocity * airControlFactor, ForceMode.Acceleration);
+//	}
+	else if (skiing) {
+		// Amplify skiing speed slightly when we start out
+		if (rigidbody.velocity.magnitude < skiingBoostBelowVelocity) {
+			rigidbody.AddForce(rigidbody.velocity * skiingBoostPercent, ForceMode.Impulse);
+		}
+		
+		// TODO change our PhysicMaterial instead
+		rigidbody.AddForce(-rigidbody.velocity.normalized * skiingDampeningFactor, ForceMode.Impulse);
 	}
 	
-	if (Input.GetMouseButton(1)) {
+	if (jetpacking) {
 		//if (grounded) {
 		//	rigidbody.velocity = Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
 		//}
 		//else {
-			rigidbody.AddForce(Physics.gravity * -2);
+		if (targetVelocity.magnitude == 0) {
+			rigidbody.AddForce(Physics.gravity * -jumpHeight);
+		}
+		else {
+			rigidbody.AddForce(Physics.gravity * (1 - fuelAirControlPenaltyFactor) * -jumpHeight);
+			rigidbody.AddForce(targetVelocity * fuelAirControlFactor);
+		}
 		//}
+	}
+	else if (!grounded) {
+		// free air control (takes no fuel)
+		rigidbody.AddForce(targetVelocity * freeAirControlFactor);
 	}
 
 	// Jump
@@ -62,13 +147,14 @@ function FixedUpdate ()
 	//}
  
 	// We apply gravity manually for more tuning control
-	rigidbody.AddForce(Vector3 (0, -gravity * rigidbody.mass, 0));
+	//rigidbody.AddForce(Vector3 (0, -gravity * rigidbody.mass, 0));
  
 	grounded = false;
 }
  
 function OnCollisionStay ()
 {
+	//Debug.Log("ON COLLISION STAY");
 	grounded = true;	
 }
  
@@ -78,3 +164,9 @@ function CalculateJumpVerticalSpeed ()
 	// for the character to reach at the apex.
 	return Mathf.Sqrt(2 * jumpHeight * gravity);
 }
+
+function AdjustGroundVelocityToNormal (hVelocity : Vector3, groundNormal : Vector3) : Vector3 {
+	var sideways : Vector3 = Vector3.Cross(Vector3.up, hVelocity);
+	return Vector3.Cross(sideways, groundNormal).normalized * hVelocity.magnitude;
+}
+
